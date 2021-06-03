f1cf0c94710e98854c5d164ad12cc1e24825ffdc
Christopher Jennings <25697451+CGJennings@users.noreply.github.com>
2021-06-03 11:34:17 -0400

    ToolCloseButton should react dynamically to bg color changes

--- src/ca/cgjennings/apps/arkham/ToolCloseButton.java
+++ src/ca/cgjennings/apps/arkham/ToolCloseButton.java
@@ -19,26 +19,44 @@ public class ToolCloseButton extends JButton {
 
     public ToolCloseButton() {
         super("");
-        if (NORMAL == null) {
-            NORMAL = ResourceKit.getIcon("ui/controls/close-inv0.png");
-            ROLLOVER = ResourceKit.getIcon("ui/controls/close-inv1.png");
-            SELECTED = ResourceKit.getIcon("ui/controls/close-inv2.png");
+        if (DK_NORMAL == null) {
+            DK_NORMAL = ResourceKit.getIcon("ui/controls/close-inv0.png");
+            DK_ROLLOVER = ResourceKit.getIcon("ui/controls/close-inv1.png");
+            DK_SELECTED = ResourceKit.getIcon("ui/controls/close-inv2.png");
+            LI_NORMAL = ResourceKit.getIcon("ui/controls/close0.png");
+            LI_ROLLOVER = ResourceKit.getIcon("ui/controls/close1.png");
+            LI_SELECTED = ResourceKit.getIcon("ui/controls/close2.png");
         }
+        setUI(new BasicButtonUI());
         super.setOpaque(true);
         setContentAreaFilled(false);
         setDefaultCapable(false);
-        setUI(new BasicButtonUI());
-        setBackground(Color.BLACK);
         setBorderPainted(false);
         setFocusPainted(false);
         setBorder(BorderFactory.createMatteBorder(0, 4, 0, 4, Color.BLACK));
         setMargin(MARGIN);
-        setIcon(NORMAL);
-        setSelectedIcon(SELECTED);
         setRolloverEnabled(true);
-        setRolloverIcon(ROLLOVER);
-        setRolloverSelectedIcon(ROLLOVER);
+        setBackground(Color.BLACK);
+    }
 
+    @Override
+    public void setBackground(Color bg) {
+        super.setBackground(bg);
+        if (bg == null) {
+            bg = getBackground();
+        }
+        final int greyness = bg == null ? 255 : (bg.getRed() + bg.getGreen() + bg.getBlue()) / 3;
+        if (greyness < 128) {
+            setIcon(DK_NORMAL);
+            setSelectedIcon(DK_SELECTED);
+            setRolloverIcon(DK_ROLLOVER);
+            setRolloverSelectedIcon(DK_ROLLOVER);
+        } else {
+            setIcon(LI_NORMAL);
+            setSelectedIcon(LI_SELECTED);
+            setRolloverIcon(LI_ROLLOVER);
+            setRolloverSelectedIcon(LI_ROLLOVER);
+        }
     }
 
     @Override
@@ -47,6 +65,6 @@ public class ToolCloseButton extends JButton {
         // this'll fix 'em
     }
 
-    private static Icon NORMAL, ROLLOVER, SELECTED;
+    private static Icon DK_NORMAL, DK_ROLLOVER, DK_SELECTED, LI_NORMAL, LI_ROLLOVER, LI_SELECTED;
     private static final Insets MARGIN = new Insets(0, 4, 0, 4);
 }
