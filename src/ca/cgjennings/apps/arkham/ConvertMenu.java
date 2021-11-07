c75212aff11a74f4839d9bbe09a3f6c1806718b1
Henrik Rostedt <henrik.rostedt@fogity.com>
2021-11-07 07:49:34 +0100

    add groups to convert menu

--- src/ca/cgjennings/apps/arkham/ConvertMenu.java
+++ src/ca/cgjennings/apps/arkham/ConvertMenu.java
@@ -7,8 +7,11 @@ import ca.cgjennings.apps.arkham.component.conversion.ConversionTrigger;
 import ca.cgjennings.apps.arkham.diy.DIY;
 import gamedata.ConversionMap;
 import gamedata.ConversionMap.ConversionEntry;
+import gamedata.ConversionMap.GroupEntry;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
@@ -17,26 +20,17 @@ import javax.swing.event.MenuListener;
 
 final class ConvertMenu extends JMenu {
 
+    private String className = null;
+    private boolean classNameUpdated = false;
+
     public ConvertMenu(JMenu parent) {
         super("app-convert");
         parent.addMenuListener(new MenuListener() {
 
             @Override
             public void menuSelected(MenuEvent e) {
-                GameComponent gc = StrangeEons.getActiveGameComponent();
-                if (gc == null) {
-                    setEnabled(false);
-                    return;
-                }
-                String className;
-                if (gc instanceof DIY) {
-                    DIY diy = (DIY) gc;
-                    className = "diy:" + diy.getHandlerScript();
-                } else {
-                    className = gc.getClass().getName();
-                }
-                removeAll();
-                createMenuItems(className);
+                updateClassName();
+                createMenuItems();
                 setEnabled(getMenuComponentCount() > 0);
             }
 
@@ -50,15 +44,44 @@ final class ConvertMenu extends JMenu {
         });
     }
 
-    private void createMenuItems(String className) {
+    private void updateClassName() {
+        GameComponent gc = StrangeEons.getActiveGameComponent();
+        String newClassName;
+        if (gc == null) {
+            newClassName = null;
+        } else if (gc instanceof DIY) {
+            DIY diy = (DIY) gc;
+            newClassName = "diy:" + diy.getHandlerScript();
+        } else {
+            newClassName = gc.getClass().getName();
+        }
+        if (newClassName == null || !newClassName.equals(className)) {
+            classNameUpdated = true;
+        }
+        className = newClassName;
+    }
+
+    private void createMenuItems() {
+        if (!classNameUpdated) {
+            return;
+        }
+        removeAll();
         ConversionMap conversionMap;
         try {
             conversionMap = ConversionMap.getGlobalInstance();
         } catch (IOException e) {
             return;
         }
+        // create group conversions
+        Map<GroupEntry, Set<ConversionEntry>> groups = conversionMap.getGroupConversions(className);
+        for (Entry<GroupEntry, Set<ConversionEntry>> entry : groups.entrySet()) {
+            JMenu groupMenu = new JMenu(entry.getKey().getName());
+            createConversionItems(entry.getValue(), groupMenu);
+            add(groupMenu);
+        }
         // create direct conversions
         createConversionItems(conversionMap.getDirectConversions(className), this);
+        classNameUpdated = false;
     }
 
     private void createConversionItems(Set<ConversionEntry> entries, JMenu parent) {
