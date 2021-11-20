d64a55e02ba692560036a1df95e63b1e84935022
Henrik Rostedt <henrik.rostedt@fogity.com>
2021-11-20 23:19:53 +0100

    set edge style to cut for thumbnails

--- src/ca/cgjennings/apps/arkham/project/MetadataSource.java
+++ src/ca/cgjennings/apps/arkham/project/MetadataSource.java
@@ -6,6 +6,7 @@ import ca.cgjennings.apps.arkham.component.ComponentMetadata;
 import ca.cgjennings.apps.arkham.component.GameComponent;
 import ca.cgjennings.apps.arkham.diy.DIY;
 import ca.cgjennings.apps.arkham.plugins.PluginBundle;
+import ca.cgjennings.apps.arkham.sheet.EdgeStyle;
 import ca.cgjennings.apps.arkham.sheet.PrintDimensions;
 import ca.cgjennings.apps.arkham.sheet.RenderTarget;
 import ca.cgjennings.apps.arkham.sheet.Sheet;
@@ -1150,7 +1151,7 @@ public class MetadataSource {
                         data[3] = String.format("%,d", sheets.length);
 
                         sheets[0].setPrototypeRenderingModeEnabled(false);
-                        BufferedImage image = sheets[0].paint(RenderTarget.FAST_PREVIEW, 72d);
+                        BufferedImage image = sheets[0].paint(RenderTarget.FAST_PREVIEW, 72d, EdgeStyle.CUT);
                         PrintDimensions dim = sheets[0].getPrintDimensions();
                         final int unit = Length.getDefaultUnit();
                         final double w = dim.getWidthInUnit(unit);
