ff150f5ba1c22054d7db5466eaba96095646f01f
Christopher Jennings <25697451+CGJennings@users.noreply.github.com>
2024-02-16 16:15:23 -0500

    some more cleanup

--- src/main/java/ca/cgjennings/apps/arkham/generic/GenericCardBackSheet.java
+++ src/main/java/ca/cgjennings/apps/arkham/generic/GenericCardBackSheet.java
@@ -1,13 +1,10 @@
 package ca.cgjennings.apps.arkham.generic;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 
 import ca.cgjennings.apps.arkham.component.DefaultPortrait;
 import ca.cgjennings.apps.arkham.sheet.RenderTarget;
 import ca.cgjennings.apps.arkham.sheet.Sheet;
-import resources.Settings;
-import resources.Settings.Region;
 
 /**
  * The sheet implementation used to render the back face of
@@ -30,13 +27,9 @@ public class GenericCardBackSheet extends Sheet<GenericCardBase>{
 
     @Override
     protected void paintSheet(RenderTarget target) {
-        final GenericCardBase card = getGameComponent();
-        final Settings s = card.getSettings();
         Graphics2D g = createGraphics();
         try {
-            g.setColor(Color.WHITE);
-            Region bleedSurface = s.getRegion(card.key("-bleed"));
-            g.fillRect(bleedSurface.x, bleedSurface.y, bleedSurface.width, bleedSurface.height);
+            // no need to fill the card area with white; the back face portrait does it for us
             backFace.paint(g, target);
         } finally {
             g.dispose();
